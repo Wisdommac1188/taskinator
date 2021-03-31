1ebbe09bc4876cb9be392d77cd965fdc71e5ab31
Travis McDonald <travis.mcdonald.tm801@gmail.com>
2021-03-31 01:47:54 -0600

    added inner html

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -3,10 +3,18 @@ let tasksToDoEl = document.querySelector("#tasks-to-do");
 
 let createTaskHandler = function(event) {
     event.preventDefault();
-
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+   
     listItemsEl = document.createElement("li");
     listItemsEl.className = "task-item";
-    listItemsEl.textContent = "This is a new task.";
+    
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemsEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemsEl);
   };
 
